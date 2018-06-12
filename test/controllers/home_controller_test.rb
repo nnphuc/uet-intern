01be0b29d2e1a2f75a47aab1627979d5d8a77fcb
Hieu Dinh <dinhhieu16598@gmail.com>
2018-06-12 16:54:40 +0700

    fix ignore

--- test/controllers/home_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get student_info" do
-    get home_student_info_url
-    assert_response :success
-  end
-
-end
--- test/system/thuctaps_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class ThuctapsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit thuctaps_url
-  #
-  #   assert_selector "h1", text: "Thuctap"
-  # end
-end
