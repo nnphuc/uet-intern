cb2d8cdab70c188a992153c6ee8d5900479bb9ac
Nguyen Ngoc Phuc <nnp.uet@gmail.com>
2018-06-12 23:12:09 +0700

    follow ajax

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,32 +1,33 @@
 class FollowsController < ApplicationController
 
-  # GET /follows
-  # GET /follows.json
-  def index
-    @follows = Follow.all
-  end
 
-  # GET /follows/1
-  # GET /follows/1.json
-  def show
-  end
-
-  # GET /follows/new
   def create
     @user = User.find(params[:followed_id])
-    current_user.follow(@user)
+    result = !current_user.follow(@user)
     respond_to do |format|
-      format.html 
-      format.js
+        format.json do 
+            render json: {
+                followed_id: @user.id,
+                follower_id: current_user.id,
+                count: current_user.followers.size,
+                sucess: result
+            }.to_json
+        end
     end
   end
 
   def destroy
-    @user = Follow.find(params[:id]).followed
-    current_user.unfollow(@user)
+    @user = User.find(params[:followed_id])
+    result = !current_user.unfollow(@user)
     respond_to do |format|
-      format.html 
-      format.js
+        format.json do 
+            render json: {
+                followed_id: @user.id,
+                follower_id: current_user.id,
+                count: current_user.followers.size,
+                sucess: result
+            }.to_json
+        end
     end
   end
 
--- app/views/follows/_follow.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! follow, :id, :follower_id, :followed_id, :created_at, :updated_at
-json.url follow_url(follow, format: :json)
